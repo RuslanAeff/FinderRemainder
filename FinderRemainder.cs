15acdfa2d6c0f5d0793f0a2924cb916ff3f3e133
Ruslan Aliyev <ruslanra@code.edu.az>
2023-07-16 18:35:16 +0400

    first commit

---
+++ FinderRemainder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qaliqfinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] numbers = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+            Console.WriteLine("The numbers which produce the remainder 0 after divided by 2 are:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                
+                if (numbers[i]%2==0)
+                {
+                        Console.Write(numbers[i]+ " ");
+                }
+            }
+           
+        }
+    }
+}
